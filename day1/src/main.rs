2aecfa24be896d9dc7c0ef376acec26bac97462d
Hugveldt <hugo.matthews@googlemail.com>
2023-08-07 19:31:01 +0100

    day 1 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let max = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|it| it.map_while(|x| x).sum1::<u64>())
+        .map(std::cmp::Reverse)
+        .k_smallest(3)
+        .map(|x| x.0)
+        .sum::<u64>();
+
+    println!("{max:?}");
 }
