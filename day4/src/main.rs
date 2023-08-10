532877298ad4b277390e7b5b69eb1934f43feb69
Hugveldt <hugo.matthews@googlemail.com>
2023-08-10 16:47:16 +0100

    day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,7 +2,13 @@ use itertools::Itertools;
 use std::ops::RangeInclusive;
 
 fn main() {
-    let redundant = include_str!("input.txt")
+    part_one();
+
+    part_two();
+}
+
+fn part_one() {
+    let completely_redundant = include_str!("input.txt")
         .lines()
         .map(|line| {
             line.split(',')
@@ -20,7 +26,30 @@ fn main() {
         .filter(|(a, b)| a.completely_overlaps(b))
         .count();
 
-    dbg!(redundant);
+    dbg!(completely_redundant);
+}
+
+fn part_two() {
+    let partially_redundant = include_str!("input.txt")
+    .lines()
+    .map(|line| {
+        line.split(',')
+            .map(|range| {
+                range
+                    .split('-')
+                    .map(|n| n.parse().expect("Range start/end should be u32"))
+                    .collect_tuple::<(u32, u32)>()
+                    .map(|(start, end)| start..=end)
+                    .expect("Each range should have a start and an end")
+            })
+            .collect_tuple::<(RangeInclusive<u32>, RangeInclusive<u32>)>()
+            .expect("Each line must have a pair of ranges")
+    })
+    .filter(|(a, b)| a.overlaps(b) || b.overlaps(a))
+    .count();
+
+    
+    dbg!(partially_redundant);
 }
 
 trait InclusiveRangeExt {
@@ -29,6 +58,8 @@ trait InclusiveRangeExt {
     fn completely_overlaps(&self, other: &Self) -> bool {
         self.contains_range(other) || other.contains_range(self)
     }
+
+    fn overlaps(&self, other: &Self) -> bool;
 }
 
 impl<T> InclusiveRangeExt for RangeInclusive<T>
@@ -37,5 +68,9 @@ where
 {
     fn contains_range(&self, other: &Self) -> bool {
         self.contains(other.start()) && self.contains(other.end())
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.start()) || self.contains(other.end())
     }
 }
