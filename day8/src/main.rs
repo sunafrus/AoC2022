c98e7460d9344c2cf8756a6241ecc07d5445c5b3
Hugveldt <hugo.matthews@googlemail.com>
2023-08-12 21:49:34 +0100

    day8 part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         (0..grid.width()).map(move |x| GridCoord::from((x, y)))
     });
 
+    /*
     let num_visible_cells = all_coords
         .filter(|&coord| {
             let coord_height = grid.cell(coord).unwrap();
@@ -26,7 +27,16 @@ fn main() {
         })
         .count();
     dbg!(num_visible_cells);
+    */
+
+    let best_place = all_coords
+        .map(|coord| (coord, scenic_score(&grid, coord)))
+        .max_by_key(|(_, score)| *score)
+        .unwrap();
+
+    println!("{best_place:?}");
 }
+
 
 fn parse_grid(input: &str) -> Grid<usize> {
     let width = input.lines().next().unwrap().len();
@@ -41,4 +51,34 @@ fn parse_grid(input: &str) -> Grid<usize> {
     }
 
     grid
+}
+
+fn visible_trees_in_dir(grid: &Grid<usize>, coord: GridCoord, (dir_x, dir_y): (isize, isize)) -> usize {
+    let line = (1..).map_while(|i| {
+        let coord = GridCoord {
+            x: coord.x.checked_add_signed(dir_x * i)?,
+            y: coord.y.checked_add_signed(dir_y * i)?,
+        };
+        Some(*grid.cell(coord)?)
+    });
+
+    let mut total = 0;
+    let our_height = *grid.cell(coord).unwrap();
+
+    for height in line {
+        total += 1;
+        if height >= our_height {
+            break;
+        }
+    }
+
+    total
+
+}
+
+fn scenic_score(grid: &Grid<usize>, coord: GridCoord) -> usize {
+    let dirs: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+    dirs.into_iter()
+        .map(|(dir_x, dir_y)| visible_trees_in_dir(grid, coord, (dir_x, dir_y)))
+        .product()
 }
